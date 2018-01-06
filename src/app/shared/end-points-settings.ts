b2bbbaa540b4863d317bd47eb59ed91ae28f7efe
pawelmalina <pawel.malina@dzielo.pl>
2018-01-06 11:48:37 +0100

    wyswietlenie widoku file-details

--- src/app/shared/end-points-settings.ts
+++ src/app/shared/end-points-settings.ts
@@ -1,9 +1,10 @@
 export class EndPointsSettings {
   // static PREFIX = 'api/';
-  static PREFIX = 'http://localh:8080/';
+  static PREFIX = 'http://localhost:8080/';
   //static END_POINT_PREFIX = 'http://localhost:3333/api/';
   // static USER = EndPointsSettings.PREFIX + 'usersData';
   static USER = EndPointsSettings.PREFIX + 'users';
   static PROJECT = EndPointsSettings.PREFIX + 'project';
+  static DOCUMENT = EndPointsSettings.PREFIX + 'document';
   // static PROJECT = EndPointsSettings.PREFIX + 'projectsData';
 }
